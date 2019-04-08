d9087a09cd081a25d3c26d1bc86ff41acca631c0
Rinita Nair <rnair3@uncc.edu>
2019-04-08 00:31:07 -0400

    Services

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Book} from './book';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class BookService {
     new Book('ABC', 'Dummy', 'https://image.dhgate.com/0x0/f2/albu/g6/M01/22/25/rBVaSFpDnAKAeB2AAABYR7tag2g240.jpg'),
     new Book('XYZ', 'Dummy', 'https://image.dhgate.com/0x0/f2/albu/g6/M01/22/25/rBVaSFpDnAKAeB2AAABYR7tag2g240.jpg')
   ];
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getBooks() {
     return this.books;
@@ -23,4 +25,13 @@ export class BookService {
   deleteBook(book: Book) {
     this.books.splice(this.books.indexOf(book), 1);
   }
+
+  // getSearchedBook(search: string) {
+  //   const encodedURI  = encodeURI('https://www.googleapis.com/books/v1/volumes?q=' + search + '&maxResults=12');
+  //   return this.http.get(encodedURI).map((response: Response) => response.json()).subscribe(
+  //     data: Book[] =>
+  //   {
+  //     this.books = data;
+  //   };
+  // }
 }
